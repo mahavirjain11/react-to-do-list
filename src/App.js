9078efd644ebc21eec13dfdebbdd695853cf706d
mahavirjain11 <mahavirjain1913@gmail.com>
2021-01-14 10:44:43 +0530

    first commit to-do-list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React , {useState,useEffect} from "react"
+import Form from "./components/Form"
+import TodoList from './components/TodoList';
 function App() {
+  // States
+  const [inputText ,setInputText] = useState("")
+  const [todos,setTodos] = useState([])
+  const [status,setStatus] = useState("All")
+  const [filterdTodos,setfilteredTodos] = useState([])
+  // Functions
+  const filterHandler = () => {
+    switch(status) {
+      case "completed" :
+        setfilteredTodos(todos.filter(todo => todo.completed===true))
+        break;
+      case "uncompleted" :
+        setfilteredTodos(todos.filter(todo => todo.completed===false))
+        break;
+      default :
+        setfilteredTodos(todos)
+        break;     
+    }
+  } 
+  // Use Effect
+    useEffect(() =>{
+      filterHandler()
+    },[todos ,status])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form 
+      inputText={inputText} 
+      todos={todos} 
+      setTodos={setTodos} 
+      setInputText={setInputText}
+      status={status}
+      setStatus={setStatus}
+      />
+
+      <TodoList 
+      todos={todos}
+      setTodos={setTodos}
+      filteredTodos={filterdTodos}
+      />
     </div>
   );
 }
 
 export default App;
+
